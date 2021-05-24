0ca12f5a2d7c3d536d480e37ff9ebb5517b06441
ChinhPV1293 <60689908+ChinhPV1293@users.noreply.github.com>
2021-05-24 16:29:49 +0700

    Add files via upload

---
+++ soccer teams/src/main/java/chinhpv/relama/RelamaApplication.java
@@ -0,0 +1,13 @@
+package chinhpv.relama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelamaApplication.class, args);
+	}
+
+}
---
+++ soccer teams/src/test/java/chinhpv/relama/RelamaApplicationTests.java
@@ -0,0 +1,13 @@
+package chinhpv.relama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
